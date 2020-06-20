85a36c8dd9aaaa67eb2d6adf0ae3f5d2eb1dbfc4
خالد سالم عبدالهادي - ١٨١ <66526530+Xhaled5@users.noreply.github.com>
2020-06-20 20:59:00 +0300

    classwork done khaled abdulhadi
    h

---
+++ ios-cw3-pt1.playground/Contents.swift
@@ -0,0 +1,7 @@
+var number1 = 12
+var number2 = 3
+func add (number1:Int, number2:Int) -> Int {
+    return   (number1 + number2)
+}
+var functionCall = add(number1: number1 , number2: number2 )
+print (functionCall)
